2378f781954aa397228da2acdced5aafe42caba2
chen <18438128833@163.com>
2019-10-23 15:05:37 +0800

    修复写入支付宝信息的API接口问题

--- utils/update_pay_key.py
+++ utils/update_pay_key.py
@@ -12,21 +12,24 @@ def write_pay_key(public,private,appid):
         public = public.strip()
         private = private.strip()
         appid = appid.strip()
-        pus = '-----BEGIN PUBLIC KEY-----'+'\n' + public + '\n' +'-----END PUBLIC KEY-----'
-        prs = '-----BEGIN PUBLIC KEY-----'+'\n' + private + '\n' + '-----END PUBLIC KEY-----'
-        with open('./utils/alipay_public_key.txt','w',encoding='utf-8') as fp1:
-            fp1.write(pus)
-            fp1.close()
-        with open('./utils/app_private_key.txt','w',encoding='utf-8') as fp2:
-            fp2.write(prs)
-            fp2.close()
-        if type(appid) == str:
-            with open('./utils/appid.txt', 'w', encoding='utf-8') as fp3:
-                fp3.write(appid)
-                fp3.close()
+        if appid.isdigit():
+            pus = '-----BEGIN PUBLIC KEY-----'+'\n' + public + '\n' +'-----END PUBLIC KEY-----'
+            prs = '-----BEGIN PUBLIC KEY-----'+'\n' + private + '\n' + '-----END PUBLIC KEY-----'
+            with open('./utils/alipay_public_key.txt','w',encoding='utf-8') as fp1:
+                fp1.write(pus)
+                fp1.close()
+            with open('./utils/app_private_key.txt','w',encoding='utf-8') as fp2:
+                fp2.write(prs)
+                fp2.close()
+            if type(appid) == str:
+                with open('./utils/appid.txt', 'w', encoding='utf-8') as fp3:
+                    fp3.write(appid)
+                    fp3.close()
+            else:
+                return 'appid 写入失败'
+            return '写入成功'
         else:
-            return 'appid 写入失败'
-        return '写入成功'
+            return 'APPID格式不正确请重新输入！'
     else:
         return '格式不正确，不是字符串'
 
