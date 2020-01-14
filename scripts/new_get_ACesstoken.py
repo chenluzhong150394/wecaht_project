83e772fe4a674ae84665308b41a3af506456bbdc
Brunce.chen <chen@MacBook-Pro.local>
2020-01-15 00:00:35 +0800

    增加获取acesstoken脚本

--- scripts/new_get_ACesstoken.py
+++ scripts/new_get_ACesstoken.py
@@ -5,17 +5,18 @@ import mysql.connector
 
 
 def data_conn(token):
-    print('wechat' + '@' + '127.0.0.1' + ':' + '3306')
+    print('开始写入acesstoken')
+    # print('wechat' + '@' + '127.0.0.1' + ':' + '3306')
     conn = mysql.connector.connect(
     host='127.0.0.1',
     port=3306,
-    user='wechat',
-    password='tenew123456',
-    charset='utf8mb4',
-    database = 'wechat',
+    user='clz',
+    password='oracle',
+    charset='utf8',
+    database = 'clz',
     )
     cursor = conn.cursor()
-    sql = """ UPDATE wechat.website_token SET token = '%s' WHERE id = 1 """ % token
+    sql = """ UPDATE clz.website_token SET token = '%s' WHERE id = 1 """ % token
     content = cursor.execute(sql)
     conn.commit()
     #data_result = list(cursor.fetchall())
@@ -25,7 +26,7 @@ def data_conn(token):
 
 
 def get_acesstoken():
-    shell = 'curl "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx915151a971f177d2&secret=a8f7e885da39a7e346827c74b5282e53"'
+    shell = 'curl "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx1e15cce8c4e924ef&secret=UMT1gZuyQUWCfe11RquFASDO6bsCJukC"'
     res = os.popen(shell).readlines()
     res = json.loads(res[0])
     token = res['access_token']
--- website/urls.py
+++ website/urls.py
@@ -4,7 +4,7 @@ from rest_framework_jwt.views import obtain_jwt_token,refresh_jwt_token
 
 urlpatterns = [
     url(r'^write_tran_record$', views.write_tran_record),
-    url(r'^wex$', views.weixin_mainbak),
+    url(r'^wex$', views.weixin_main),
     url(r'^login$', views.login),
     url(r'^test$', views.test),
 ]
